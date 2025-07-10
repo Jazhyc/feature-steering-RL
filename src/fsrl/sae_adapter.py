8224d057ce8446ca69c14b65c7de016f00cac8b5
JazHyc <feronest@yahoo.com>
2025-07-10 05:29:17 +0200

    Progress towards making explicit adapter class

--- src/fsrl/sae_adapter.py
+++ src/fsrl/sae_adapter.py
@@ -1,4 +1,16 @@
-from sae_lens import SAE
+from sae_lens import SAE, SAEConfig
+from transformer_lens.hook_points import HookPoint
 
 class SAEAdapter(SAE):
-    pass
+    
+    def __init__(self, cfg: SAEConfig, use_error_term: bool = False):
+        
+        # Use the explicit form of super() to avoid the metaclass conflict
+        super(SAEAdapter, self).__init__(cfg, use_error_term=use_error_term)
+        
+        # Freeze all parameters currently in the SAE
+        for param in self.parameters():
+            param.requires_grad = False
+        
+        # Set up extra adapter hook
+        self.hook_sae_adapter = HookPoint()
