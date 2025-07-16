4f9ce2b9efdd7a30abc0ec9dbb3e7afcf0946cb6
matthijs <mattijs.vanderlende@novigenix.com>
2025-07-16 14:45:47 +0200

    added basic class nothing much yet

---
+++ src/util/saefeatureanalyzer.py
@@ -0,0 +1,17 @@
+from fsrl.sae_adapter import SAEAdapter
+
+
+class SAEfeatureAnalyzer:
+    """
+    This class takes in an SAEAdapter and can be used to inspect
+    its features and the steering vector of the policy.
+    """
+    def __init__(sae_adapter: SAEAdapter):
+        self.sae_adapter = sae_adapter
+
+    def get_feature_description():
+        pass
+
+    def create_viz() -> None:
+        pass
+
