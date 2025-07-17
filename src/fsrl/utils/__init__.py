3ac2f137cc0d369baf5600c783d6f6f611156d32
JazHyc <feronest@yahoo.com>
2025-07-17 17:41:24 +0200

    Moved utils into library and created debugging notebook

---
+++ src/fsrl/utils/__init__.py
@@ -0,0 +1,1 @@
+from .saefeatureanalyzer import SAEfeatureAnalyzer
--- src/fsrl/utils/saefeatureanalyzer.py
+++ src/fsrl/utils/saefeatureanalyzer.py
@@ -18,7 +18,10 @@ class SAEfeatureAnalyzer:
         self.input = None
         self.sae = self.hooked_model.sae_adapter
         self.model_id = self.sae.cfg.model_name
-        self.sae_id = self.sae.cfg.sae_id
+        
+        # The name of the model is a bit different in Neuronpedia
+        self.sae_id = self.sae.cfg.neuronpedia_id.split('/')[-1]
+        
         self.html_template = "https://neuronpedia.org/{}/{}/{}?embed=true&embedexplanation=true&embedplots=true&embedtest=true&height=300"
         
         os.environ["NEURONPEDIA_API_KEY"] = api_key   # Not sure if the request here require the API key
@@ -28,10 +31,8 @@ class SAEfeatureAnalyzer:
         self._collect_feature_labels()
 
     def _collect_feature_labels(self) -> None:
-        # For some reason I keep getting error code 500
-        # (https://www.reddit.com/r/CharacterAI/comments/15vjbsh/serious_question_what_is_the_500_server_error_and/)
         for idx in range(self.sae.cfg.d_sae):
-            self.feature_info[idx] = SAEFeature.get(self.model_id, self.sae_id, str(idx))
+            self.feature_info[idx] = SAEFeature.get(self.model_id, self.sae_id, idx)
     
     def _get_dashboard_html(self, sae_release: str, sae_id: str, feature_idx: int):
         return self.html_template.format(sae_release, sae_id, feature_idx)
