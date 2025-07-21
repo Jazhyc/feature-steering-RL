65c235bdbea9036c88132b60d0f14676020380b4
JazHyc <feronest@yahoo.com>
2025-07-21 18:55:11 +0200

    Reverted unnecessary chat template addition

--- src/fsrl/__init__.py
+++ src/fsrl/__init__.py
@@ -1,11 +1,10 @@
 from .sae_adapter import SAEAdapter
 from .hooked_model import HookedModel
-from .simPO import SimPOTrainer, SimPOConfig, apply_chat_template
+from .simPO import SimPOTrainer, SimPOConfig
 
 __all__ = [
     "SAEAdapter",
     "HookedModel",
     "SimPOTrainer",
     "SimPOConfig",
-    "apply_chat_template",
 ]
